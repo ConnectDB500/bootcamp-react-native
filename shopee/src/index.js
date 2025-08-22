e720754addf792ed30def6d7cea97cdf5d124b66
hackerroot <liberalinoenterprise500@gmail.com>
2025-08-22 03:24:14 -0300

    update

--- shopee/src/index.js
+++ shopee/src/index.js
@@ -12,8 +12,8 @@ const item2 = await createItem("mouse", 19.99, 1);
 await cartService.addItem(myCart, item1);
 // await cartService.addItem(myCart, item2);
 
-await cartService.removeItem(myCart, 2);
-await cartService.removeItem(myCart, 1)
+await cartService.removeItem(myCart, item2);
+await cartService.removeItem(myCart, item1)
 
 await cartService.displayCart(myCart);
 
